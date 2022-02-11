484dcb30f70c07215b1f0ff38926097679b1357e
zQuantz <zach.barillaro@gmail.com>
2022-02-10 20:10:13 -0500

    updated download

--- db/download.sh
+++ db/download.sh
@@ -1,13 +1,13 @@
 gsutil -m cp gs://clean_items/news/* $HOME/NewsLab/db/tdata/news/
 gsutil -m cp gs://clean_items/tweets/* $HOME/NewsLab/db/tdata/tweets/
 
-files="$HOME/NewsLab/db/tdata/news"
+files="$HOME/NewsLab/db/tdata/news/*"
 for f in $files
 do
 	tar xvf $f -C $HOME/NewsLab/db/cdata/news
 done
 
-files="$HOME/NewsLab/db/tdata/tweets"
+files="$HOME/NewsLab/db/tdata/tweets/*"
 for f in $files
 do
 	tar xvf $f -C $HOME/NewsLab/db/cdata/tweets
