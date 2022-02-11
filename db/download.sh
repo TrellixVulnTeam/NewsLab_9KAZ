1f9f1a34adcef91a9bb3417edd7d7993efee48a9
zQuantz <zach.barillaro@gmail.com>
2022-02-10 19:55:41 -0500

    added download script

---
+++ db/download.sh
@@ -0,0 +1,17 @@
+gsutil -m cp gs://clean_items/news/* $HOME/NewsLab/db/tdata/news/
+gsutil -m cp gs://clean_items/tweets/* $HOME/NewsLab/db/tdata/tweets/
+
+files="$HOME/NewsLab/db/tdata/news"
+for f in $files
+do
+	tar xvf $f -C $HOME/NewsLab/db/cdata/news
+done
+
+files="$HOME/NewsLab/db/tdata/tweets"
+for f in $files
+do
+	tar xvf $f -C $HOME/NewsLab/db/cdata/tweets
+done
+
+rm -rf $HOME/NewsLab/db/tdata/tweets/*
+rm -rf $HOME/NewsLab/db/tdata/news/*
