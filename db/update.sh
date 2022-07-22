576f61d771434a5cc60c47d3c40839f51e57fa84
zQuantz <zach.barillaro@gmail.com>
2022-07-21 20:59:42 -0400

    added update script

---
+++ db/update.sh
@@ -0,0 +1,14 @@
+gsutil cp gs://clean_items/news/2022-07-21.tar.xz $HOME/NewsLab/db/tdata/news/
+gsutil cp gs://clean_items/tweets/news_2022-07-21 00-00-01 $HOME/NewsLab/db/tdata/tweets/
+
+files="$HOME/NewsLab/db/tdata/news/*"
+for f in $files
+do
+	tar xvf "$f" -C $HOME/NewsLab/db/cdata/news
+done
+
+files="$HOME/NewsLab/db/tdata/tweets/*"
+for f in $files
+do
+	tar xvf "$f" -C $HOME/NewsLab/db/cdata/tweets
+done
